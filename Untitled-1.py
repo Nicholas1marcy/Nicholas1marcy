ff5265eb7d737204b966c00de84f4c51ec26f65f
Marcy Truong <nicholastruong0526@gmail.com>
2021-10-27 20:41:00 -0400

    I created this code a

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+import random
+agedcheese=[]
+cheeses = ["chedder","gouda","parmasean","american","mozzerella"]
+age = ["no year","1 year","2 year","3 year","4 year"]
+for x in cheeses:
+    for y in age:
+     agedcheese.append(x+" "+y)
+     
+randomcheese=random.choice(agedcheese)
+print (randomcheese)
+    
---
+++ deck = [].py
@@ -0,0 +1,22 @@
+import random
+deck = []
+
+
+suit = ["club","spade","diamond","heart"]
+numbers = [2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"]
+
+for x in suit:
+  for y in numbers:
+    print (x,y)
+    
+    player1=[]
+    player2=[]
+
+random.deck 
+
+winner = False
+
+while winner = False
+  
+
+    
