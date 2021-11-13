a9415c26a7cfc9cb9ce9400b3bece50745c8f4da
Marcy Truong <nicholastruong0526@gmail.com>
2021-11-13 13:01:33 -0500

    dinosaur practice

---
+++ dinosaurs.py
@@ -0,0 +1,14 @@
+def trex():
+  print("Roars, loudly")
+  print("Roar causes fear towards smaller dinosaurs")
+
+def dodobird():
+ if dinosaurs == trex: print("the dodobird runsaway")
+
+class dinosaurs:
+  predator=trex 
+  prey=dodobird
+ 
+dinos = dinosaurs()
+
+print (dodobird)
